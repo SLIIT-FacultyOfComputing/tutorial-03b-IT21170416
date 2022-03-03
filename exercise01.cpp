a9648397b1212597b96e347950e0a65422e9ee15
Pemarathna I.R.C <93403236+IT21170416@users.noreply.github.com>
2022-03-03 17:26:52 +0000

    update

--- exercise01.cpp
+++ exercise01.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "\tNo\tName\tMarks" << endl;
+ cout << "\tNo\t\t\tName\tMarks" << endl;
  for (int r = 0; r < 5; r++) {
      cout
           <<setw(5)
